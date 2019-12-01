36d0c8e6bdb2886cf670427404815bebdb0dccb5
Zhengyu Chen <zhengyu.chen@eavisiontech.com>
2019-12-01 14:54:08 -0800

    update exptimax_with_astar.py

--- search.py
+++ search.py
@@ -1,6 +1,7 @@
 from util import manhattanDistance
 import environment
 import copy
+import astar
 
 class ppp():
   def __init__(self,map_dim=(3, 3), block_area=None, start=(1, 1)):
@@ -47,20 +48,57 @@ def local_map_approx_search(aaa):
               f=tempt
               astore=a
       return astore
+  obstacles = list(aaa.env.block_area)
+  occupancy = astar.DetOccupancyGrid2D(aaa.env.map.width, aaa.env.map.height, obstacles)
   while(aaa.end()!=1):
       pp=ppp()
       pp.env.map.data=aaa.env.local_map(aaa.lmapsize,aaa.lmapsize)
       a=getAction(pp,aaa.lmapsize*aaa.lmapsize-1)
-      aaaa.append(aaa.env.agent_location())
-      aaa.env.step(a)
+      X,Y = aaa.env.next_location(a)
+      m = aaa.env.entire_map()
+      if m[X][Y] == aaa.env.map.VISITED:
+        newx,newy = aaa.env.remaining_nodes()[0]
+        x_init = aaa.env.agent_location()
+        aaa.env.agentX = newx
+        aaa.env.agentY = newy
+        aaa.env.map.visit(newx,newy)
+        x_goal = aaa.env.agent_location()
+        Astar = astar.AStar((0, 0), (aaa.env.map.width, aaa.env.map.height), x_init, x_goal, occupancy)
+        if not Astar.solve():
+          print("Not Solve")
+        else:
+          aaa.env.agent_distance += len(Astar.path)
+          for j in range(len(Astar.path)-1):
+            a1,b1 = Astar.path[j]
+            a2,b2 = Astar.path[j+1]
+            if a1 != a2 and b1 != b2:
+              aaa.env.agent_turns += 1
+        aaa.env.path.extend(Astar.path)
+      # aa.append(a.env.agent_location())
+      else:
+        aaaa.append(aaa.env.agent_location())
+        aaa.env.step(a)
       #print(aaaa)
-  aaaa.append(aaa.env.agent_location())
+  # aaaa.append(aaa.env.agent_location())
   return aaaa
 
 def main():
-  a=ppp((5,5),None,(0,0))
+  # a=ppp((5,5),None,(0,0))
+  a = ppp((10,10),
+                            ( ((1,1), (1,2)),
+                              ((1,2), (3,2)),
+                              ((6,1), (8,1)),
+                              ((8,1), (8,3)),
+                              ((6,3), (8,3)),
+                              ((3,4), (4,4)),
+                              ((4,4), (4,5)),
+                              ((6,6), (6,7)),
+                              ((1,7), (2,8)),
+                            ),
+                          (0,0))
   print(local_map_approx_search(a))
   print(a.env.agent_distance,a.env.agent_turns)
+  a.env.plot_path()
 
 
 
